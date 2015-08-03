955d861958714e92ed9b4a785c675186792a96cb
Amro Shohoud <shohouda@gmail.com>
2015-08-03 10:57:46 -0500

    simple modifications to scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import sys
 from lxml import html
 import requests
 
@@ -6,9 +7,13 @@ try:
     page = requests.get('https://en.wikipedia.org/wiki/' + subject)
     tree = html.fromstring(page.text)
     links = tree.xpath('//*[@id="mw-content-text"]/p[1]/a[1]/@href')
+    if (len(links) == 0):
+        page = reqeuests.get('https://en.wikipedia.org/w/index.php?search=' + subject)
+        tree = html.fromstring(page.text)
+        links = tree.xpath('//*[@class="mw-search-result-heading'[1])
     title_xpath = tree.xpath('//*[@id="firstHeading"]/text()')
     count = 0
-except ValueError:
+except:
     print "sorry page not found"
 
 while (title_xpath[0] != 'Philosophy'):  
@@ -18,6 +23,6 @@ while (title_xpath[0] != 'Philosophy'):
         links = tree.xpath('//*[@id="mw-content-text"]/p[1]/a[1]/@href')
         title_xpath = tree.xpath('//*[@id="firstHeading"]/text()')
         count += 1
+        print count, title_xpath[0]
     except:
         links = tree.xpath('//*[@id="mw-content-text"]/ul[1]/li[1]/a/@href')  
-print count
