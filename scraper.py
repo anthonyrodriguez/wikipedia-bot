1d78b0a7af1d18ad89e9bb78f3d798e0afd2e77d
Amro Shohoud <shohouda@gmail.com>
2015-08-04 14:53:24 -0500

    Fixed exceptions and irregular links (bold/italicized/wiktionary)

--- scraper.py
+++ scraper.py
@@ -1,8 +1,11 @@
 import sys
 from lxml import html
+import re
 import requests
 
-while (True):
+should_continue = True
+
+while (should_continue):
     try:
         subject = raw_input('Enter wikipedia page: ') #TODO what happens if page does not exist?
         page = requests.get('https://en.wikipedia.org/wiki/' + subject)
@@ -12,6 +15,7 @@ while (True):
             page = requests.get('https://en.wikipedia.org/w/index.php?search=' + subject)
             tree = html.fromstring(page.text)
             links = tree.xpath('//*[@class="mw-search-result-heading'[1])
+            print links
         title_xpath = tree.xpath('//*[@id="firstHeading"]/child::*/text()|//*[@id="firstHeading"]/text()')
         count = 0
     except:
@@ -19,12 +23,16 @@ while (True):
     
     while (''.join(title_xpath) != 'Philosophy'):  
         try:
-            page = requests.get('https://en.wikipedia.org' + links[0])
+            for link in links:
+                if link.split('/')[1] == 'wiki':
+                    chosen_link = link
+                    break
+            page = requests.get('https://en.wikipedia.org' + chosen_link)
             tree = html.fromstring(page.text)
 	    links = tree.xpath('//*[@id="mw-content-text"]/p/a/@href|//*[@id="mw-content-text"]/p/i/a/@href')
             title_xpath = tree.xpath('//*[@id="firstHeading"]/child::*/text()|//*[@id="firstHeading"]/text()')
             count += 1
             print count, ''.join(title_xpath)
         except:
-	    print "ERROR HERE"
-	    links = tree.xpath('//*[@id="mw-content-text"]/p/a/@href|//*[@id="mw-content-text"]/p/i/a/@href')
+            links = tree.xpath('//*[@id="mw-content-text"]/p/b/a/@href | //*[@id="mw-content-text"]/ul[1]/li[1]/a/@href')
+    should_continue = True if raw_input("Perform another search? (y/n): ") is 'y' else False
