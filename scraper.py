f4e0d6ec42941920850969329e5a286be3170c02
Anthony Rodriguez <antrodriguez@utexas.edu>
2015-08-04 16:08:46 -0500

    Forced a search when subject is not found.

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import sys
 from lxml import html
 import re
 import requests
+import pdb
 
 should_continue = True
 
@@ -11,11 +12,11 @@ while (should_continue):
         subject = raw_input('Enter wikipedia page: ') #TODO what happens if page does not exist?
         page = requests.get('https://en.wikipedia.org/wiki/' + subject)
         tree = html.fromstring(page.text)
-        links = tree.xpath('//*[@id="mw-content-text"]/p/a/@href|//*[@id="mw-content-text"]/p/i/a/@href')
+        links = tree.xpath('//*[@id="mw-content-text"]/p/a/@href|//*[@id="mw-content-text"]/p/i/a/@href | //*[@id="mw-content-text"]/p/b/a/@href | //*[@id="mw-content-text"]/ul[1]/li[1]/a/@href')
         if (len(links) == 0):
-            page = requests.get('https://en.wikipedia.org/w/index.php?search=' + subject)
+            page = requests.get('https://en.wikipedia.org/w/index.php?search={}&fulltext=Search'.format(subject))
             tree = html.fromstring(page.text)
-            links = tree.xpath('//*[@class="mw-search-result-heading'[1])
+            links = tree.xpath('//*[@class="mw-search-result-heading"]/a/@href')
         title_xpath = tree.xpath('//*[@id="firstHeading"]/child::*/text()|//*[@id="firstHeading"]/text()')
         count = 0
     except:
@@ -34,5 +35,8 @@ while (should_continue):
             count += 1
             print count, ''.join(title_xpath)
         except:
-            links = tree.xpath('//*[@id="mw-content-text"]/p/b/a/@href | //*[@id="mw-content-text"]/ul[1]/li[1]/a/@href')
+		print "WTF weren't supposed to get here"
+		pdb.set_trace()
+		break
+
     should_continue = True if raw_input("Perform another search? (y/n): ") is 'y' else False
